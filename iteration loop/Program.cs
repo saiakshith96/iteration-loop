e8a351e424f913afc1b02ddccced62fb295196b2
Sai Akshith Thodupunuri <tsaiakshith@gmail.com>
2019-09-10 20:49:19 -0400

    Add project files.

---
+++ iteration loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace iteration_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your years of experience");
+            int a = int.Parse(Console.ReadLine());
+            int i=0;
+            while(i < a){
+
+                Console.WriteLine("You have {0} years of experinece", a);
+                i++;
+
+            }
+        }
+    }
+}
