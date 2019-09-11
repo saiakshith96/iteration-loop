cfe85d5ac33b6f5cde5b19fcdc1ccdcfe9d2eba1
Sai Akshith Thodupunuri <tsaiakshith@gmail.com>
2019-09-10 20:58:12 -0400

    try catch included

--- iteration loop/Program.cs
+++ iteration loop/Program.cs
@@ -6,14 +6,22 @@ namespace iteration_loop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter your years of experience");
-            int a = int.Parse(Console.ReadLine());
-            int i=0;
-            while(i < a){
+            try
+            {
+                Console.WriteLine("enter your years of experience");
+                int a = int.Parse(Console.ReadLine());
+                int i = 0;
+                while (i <= a)
+                {
 
-                Console.WriteLine("You have {0} years of experinece", i);
-                i++;
+                    Console.WriteLine("You have {0} years of experinece", i);
+                    i++;
 
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a valid integer");
             }
         }
     }
